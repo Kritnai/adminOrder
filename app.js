fc72e81361e3720056cb7552a39ad6321aad9a33
Kritsanai Hoisang <ksnn@Kritsanais-MacBook-Pro.local>
2024-10-12 14:19:52 +0700

    1

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = require('./routes/MyAPI');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.use(express.urlencoded({ extended: false }))
+app.use(router);
+
+
+app.listen(8080, () => {
+    console.log("Starting serever at port 8080");
+})
